74f4e72db59930560f9c456c8675da9c3bc955d3
R. Stanley Hum <r.stanley.hum@gmail.com>
2017-08-02 06:11:27 -0400

    changed secrets file name to match other projects

--- app/survalytics/services/aws.js
+++ app/survalytics/services/aws.js
@@ -9,7 +9,7 @@ import { GetAllQuestions, DeleteQuestionsByGuid, DeleteQuestion, GetQuestion, In
 import { NewQuestion, DeleteQuestionGUID } from './question';
 
 
-var awsConstants = require('../assets/secrets.json');
+var awsConstants = require('../assets/aws.secret.json');
 
 var myCredentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: awsConstants.IDENTITY_POOL_ID,
